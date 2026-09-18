4da0fecd6ce11b20ff4331801850870cbad2f060
EDUARDOMUN68 <oscaredupm@hotmail.com>
2026-09-18 21:36:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='madridista-v3-11-catalog';
+const CACHE='madridista-v3-12-catalog';
 const STATIC=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 
 self.addEventListener('install',e=>{
